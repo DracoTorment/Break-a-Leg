113b5b04bb8d28544f655108f57d58794d483576
Leon Chen <leonc1516@gmail.com>
2022-04-30 07:08:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var c = canvas.getContext('2d');
-c.fillRect(100, 100, 100, 100);
+let canvas = document.querySelector('canvas')
+let c = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 30
+        this.height = 30
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update() {
+        this.draw()
+    }
+}
+class Platform {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0
+        }
+        this.width = 100
+        this.height = 30
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update() {
+        this.draw()
+    }
+}
+
+let player = new Player()
+player.draw()
+let platform = new Platform()
+platform.draw()
+
+function animate() {
+    requestAnimationFrame(animate)
+    
+}
