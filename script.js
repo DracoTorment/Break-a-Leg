516260eb4dcbaacda6b34420adb7bf02242d789d
DracoTorment <79618916+DracoTorment@users.noreply.github.com>
2022-04-30 14:18:59 -0700

    Wills script that works if zach doesnt merge

--- script.js
+++ script.js
@@ -2,113 +2,20 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext("2d");
 
-=======
-let canvas = document.querySelector('canvas')
-let c = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-
-class Player {
-    constructor() {
-        this.position = {
-            x: 100,
-            y: 100
-        }
-        this.velocity = {
-            x: 0,
-            y: 1
-        }
-        this.width = 30
-        this.height = 30
-    }
-    draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-    update() {
-        this.draw()
-    }
-}
-class Platform {
-    constructor() {
-        this.position = {
-            x: 0,
-            y: 0
-        }
-        this.width = 100
-        this.height = 30
-    }
-    draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-    update() {
-        this.draw()
-    }
-}
-
-let player = new Player()
-player.draw()
-let platform = new Platform()
-platform.draw()
-
-function animate() {
-    requestAnimationFrame(animate)
-
-}
-animate()
-
-addEventListener('keydown', ({ key }) => {
-    switch (key) {
-        case 'a':
-            console.log('left')
-            break
-        case 's':
-            console.log('down')
-            break
-        case 'd':
-            console.log('right')
-            break
-        case 'w':
-            console.log('up')
-            break
-    }
-})
-
-addEventListener('keyup', ({ key }) => {
-    switch (key) {
-        case 'a':
-            console.log('left')
-            break
-        case 's':
-            console.log('down')
-            break
-        case 'd':
-            console.log('right')
-            break
-        case 'w':
-            console.log('up')
-            break
-    }
-})
-
-
 canvas.width = 240*8;
 canvas.height = 96*8;
 const background = new Image();
-background.src = 'assests/stage_1.png';
+background.src = 'assests/stage_0.png';
 const up = new Image();
-up.src =  'assests/MainCup.png';
+up.src =  'assests/Dance/MainCup.png';
 const down = new Image();
-down.src =  'assests/MainCdown.png';
+down.src =  'assests/Dance/MainCdown.png';
 const left = new Image();
-left.src =  'assests/MainCleft.png';
+left.src =  'assests/Dance/MainCleft.png';
 const right = new Image();
-right.src =  'assests/MainCleft-1.png';
-
+right.src =  'assests/Dance/MainCright.png';
 const reg = new Image();
-reg.src = 'assests/MainCsideF2.png';
+reg.src = 'assests/Walking/MainCsideF2.png';
 const A = new Image();
 A.src = 'assests/a_plus.png'
 var key;
@@ -190,41 +97,7 @@ function animate(){
     nxt();
     arrows();
     requestAnimationFrame(animate);
-
-var key;
-
-function waitingKeypress() {
-    return new Promise((resolve) => {
-      document.addEventListener('keydown', onKeyHandler);
-      function onKeyHandler(e) {
-        if (e.keyCode === 13) {
-          document.removeEventListener('keydown', onKeyHandler);
-          resolve();
-        }
-      }
-    });
-  }
-
-// function characterstate(){
-//     if(key == 1){
-
-//     }
-//     else if(){}
-// }
-
-// function nxt(){
-
-
-//}
-
-function animate(){
-    c.drawImage(background, 0, 0, canvas.width, canvas.height);
-    //characterstate();
-    //nxt();
-    requestAnimationFrame(animate);
-    //await waitingKeypress();
-
- }
+}
 
  animate();
 
@@ -252,8 +125,5 @@ function animate(){
             sub = 1;
             break;
      }
- })
-
-     console.log(keyCode)
  })
 
