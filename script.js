5470dc9d1588eb303fbc0c5f2dd7c5ebc9c68a48
DracoTorment <79618916+DracoTorment@users.noreply.github.com>
2022-04-30 03:38:02 -0700

    image load

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-var c = canvas.getContext('2d');
-c.fillRect(100, 100, 100, 100);
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext("2d");
+
+canvas.width = 240*8;
+canvas.height = 96*8;
+const background = new Image();
+background.src = 'assests/stage_1.png';
+
+
+// function animate(){
+background.addEventListener('load', e => {
+    c.drawImage(background, 0, 0, canvas.width, canvas.height);
+});
+
+//   c.fillStyle = 'blue'
+//   c.fillRect(100,100,100,100)
+//     requestAnimationFrame(animate);
+// }
